cd18a65d2c158032fe8c9a7991a3534d4139ac32
manatsanan0209 <117584178+manatsanan0209@users.noreply.github.com>
2024-11-19 13:41:01 +0000

    feat: zoo

--- test_zoo.py
+++ test_zoo.py
@@ -7,8 +7,25 @@ class TestZoo(unittest.TestCase):
 
     def test_child_ticket_price(self):
         self.assertEqual(self.zoo.get_ticket_price(5), 50)
-       
-    # Add your additional test cases here.
+    
+    #age < 0
+    def test_lowerthanzero(self):
+        self.assertEqual(self.zoo.get_ticket_price(-1),"error")
 
+    #0 <= age <=12
+    def test_b2(self):
+        self.assertEqual(self.zoo.get_ticket_price(0),50)
+
+    #13 <= age <= 20
+    def test_b3(self):
+        self.assertEqual(self.zoo.get_ticket_price(20),100)
+
+    #21 <= age <= 60
+    def test_b4(self):
+        self.assertEqual(self.zoo.get_ticket_price(21),150)
+    
+    #Age > 60
+    def test_b5(self):
+        self.assertEqual(self.zoo.get_ticket_price(75),100)
 if __name__ == '__main__':
     unittest.main()
